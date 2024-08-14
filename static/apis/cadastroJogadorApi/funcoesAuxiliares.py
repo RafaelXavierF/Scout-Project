2ee32eacda76d5176aca8300d1ee16a6e9b25387
RafaelXavierF <rafaelxfranco.rx@gmail.com>
2024-08-14 19:58:35 -0300

    Api Clientes: Removido retorno indevido de parâmetros de cross.

--- static/apis/cadastroJogadorApi/funcoesAuxiliares.py
+++ static/apis/cadastroJogadorApi/funcoesAuxiliares.py
@@ -3,32 +3,17 @@ import json
 def formata_retorno_sucesso_corpo(status: int, evt: dict):
     return {
         "statusCode": status,
-        "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Methods": 'POST',
-            "Access-Control-Allow-Origin": '*'
-        },
         "corpo": json.dumps(evt)
     }
 
 def formata_retorno_sucesso(status: int, mensagem: str):
     return {
         "statusCode": status,
-        "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Methods": 'POST',
-            "Access-Control-Allow-Origin": '*'
-        },
         "mensagem": mensagem
     }
 
 def formata_retorno_erro(status: int, mensagem: str):
     return {
         "statusCode": status,
-        "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Methods": 'POST',
-            "Access-Control-Allow-Origin": '*'
-        },
         "mensagem": mensagem
     }
